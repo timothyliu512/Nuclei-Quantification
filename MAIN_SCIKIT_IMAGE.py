71cb3d75b3c18b4876b0ac74d10da5f48b5f763f
Timothy Liu <timothy.liu512@gmail.com>
2023-05-25 14:59:52 -0700

    Pixel intensity histogram

--- MAIN_SCIKIT_IMAGE.py
+++ MAIN_SCIKIT_IMAGE.py
@@ -35,17 +35,30 @@ HEIGHT = 10
 WIDTH = 10
 DPI = 200
 
-fig, ax = plt.subplots(1, 1, figsize=(HEIGHT, WIDTH), dpi=DPI)
-ax.imshow(image, cmap='gray')
+fig1, ax1 = plt.subplots(figsize=(HEIGHT, WIDTH), dpi=DPI)
+
+ax1.imshow(image, cmap='gray')
 
 # Draw circles on top of original image for each blob(nuclei) detected
 for blob in blobs:
     y, x, area = blob
-    ax.add_patch(plt.Circle((x, y), area*np.sqrt(2), color='r', fill=False))
+    ax1.add_patch(plt.Circle((x, y), area*np.sqrt(2), color='r', fill=False))
+
+plt.show()
 
 # Display the image with blobs(nuclei) highlighted
-plt.title(f'Number of Nuclei found: {len(blobs)}')
-plt.savefig(FILEOUTPUT)                 #save as jpg
-#plt.show()
+# plt.title(f'Number of Nuclei found: {len(blobs)}')
+# plt.savefig(FILEOUTPUT)                 #save as jpg
+
+
+# Histogram
+fig2, ax2 = plt.subplots(figsize=(HEIGHT, WIDTH), dpi=DPI)
+
+# Histogram of pixel intensity
+ax2.hist(image.ravel(), bins=256, color='gray')
+ax2.set_title('Histogram of Pixel Intensities')
+
+plt.show()
+
 print("Success!")
 
